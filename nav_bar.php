54ea557b2734166b0d08dce9b94727471ae5896a
cnpengwei <cnpengwei@gmail.com>
2013-05-15 18:39:44 +0900

    Create nav_bar.php

---
+++ nav_bar.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+  <title>nav bar</title>
+	<script src="../js/jquery-1.8.2.js" type="text/javascript"></script>
+	<script src="../js/jquery-ui-1.9.1.custom.js" type="text/javascript"></script>
+  <link href="../css/jquery-ui-1.9.2.custom.css" rel="stylesheet" type="text/css" />
+	<link href="/s/wlflag.ico" rel="icon">
+	<link href="../css/css.css" rel="stylesheet" type="text/css"></link>
+	<link href="../css/css2.css" rel="stylesheet" type="text/css"></link>
+	<link href="../css/css3.css" rel="stylesheet" type="text/css"></link>
+	<link href="../css/css4.css" rel="stylesheet" type="text/css"></link>
+	<style type="text/css">
+	
+	li{
+		text-align: -webkit-match-parent;
+	}
+	.sw_tb{
+		padding:11px 0 11px 8px;
+	}	
+	.sw_tb ul.sc_hl1 li {
+		display: inline;
+	}
+	.sw_tb ul.sc_hl1 li a{margin:0 10px 0 9px;color:#999;font-size:1em;font-weight:700;zoom:1}
+	.hp_sw_hdr{		
+		position:absolute; 
+		bottom:1in; 
+		left:3in; 
+		right:1in;
+		top:1in; 
+	}
+	#span_login{
+		border: 1px dotted #fff;
+		padding: 10px;
+	}
+	</style>
+
+</head>
+	<div id="hp_sw_hdr" class="sc_hl1">
+		<div class="sw_tb">
+			<ul id="sc_hdu" class="sc_hl1">
+				<li id="scpt0" class="sc_active">
+					<a href="" onclick="">Song</a>
+					<div id="scpc0"></div>
+				</li>
+				<li id="scpt1">
+					<a href="#" onclick="">Image</a>
+					<div id="scpc1"></div> 
+				</li>
+				<li id="scpt2">
+					<a href="#" onclick="">Video</a>
+					<div id="scpc2"></div>
+				</li>
+				<li id="scpt3" class="">
+					<a href="#" onclick="" h="ID=SERP,5014.1">Search</a>
+					<div id="scpc3"></div>
+				</li>
+			</ul>
+			
+			<span class="sw_mktsw" id="sw_mktsw" mkt="en-us" style="right: 128px; display: block; visibility: visible;">
+				<a href="#" h="ID=SERP,5000.1"> English </a>
+				<span>|</span>
+				<span id="span_login"><a href="#" id="logbtn">Log</a></span>
+			</span>
+			
+		 	 
+		</div>
+	</div>
+	
+
+
+
