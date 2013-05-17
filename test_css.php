2ff8aef96aa5f96ae94f4456fd711d2954f0eb9a
cnpengwei <cnpengwei@gmail.com>
2013-05-17 17:28:38 +0800

    Create test_css.php

---
+++ test_css.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <style type="text/css">
+	.fancy td {
+		color: #f60;
+		background: #666;
+	}
+
+	td.fancy {
+		color: #f60;
+		background: #666;
+	}
+	</style>
+
+</head>
+
+<body>
+
+<table class="fancy">
+	<tr>
+		<td>
+			me in td text<a href="#" >me link</a>
+		</td>
+	</tr>
+</table>
+<hr/>
+
+<table border="1px">
+	<tr>
+		<td class="fancy">
+			me in td text<a href="#" >me link</a>
+		</td>
+		<td>
+			common text
+		</td>
+	</tr>
+</table>
+</body>
+</html>
